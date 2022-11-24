a926e3c1bc120af37b9a46c50da1fa04e22d07e4
wallace fares <wallacefares@gmail.com>
2022-11-24 11:36:04 -0300

    fix ide

--- lib/Grid.hs
+++ lib/Grid.hs
@@ -7,19 +7,16 @@ data Vec a = Vec Int Int a deriving (Show, Eq)
 sqrtLength :: (Foldable f, Integral b) => f a -> b
 sqrtLength = round . sqrt . fromIntegral . length
 
-class (Functor f, Functor v) => Grid f v where
-  get :: Foldable f => f a -> v b -> a
-  toV :: Int -> f a -> v ()
-  fromV :: v b -> f a -> Int
-  makeGrid :: Int -> f (v ())
-  (<<$>>) :: (a -> b) -> f (v a) -> f (v b)
+class (Functor f, Functor g) => Grid f g where
+  get :: Foldable f => f a -> g b -> a
+  toV :: Int -> f a -> g ()
+  fromV :: g b -> f a -> Int
+  makeGrid :: Int -> f (g ())
 
 instance Functor Vec where
   fmap f (Vec x y a) = Vec x y (f a)
 
 instance Grid [] Vec where
-  (<<$>>) = fmap . fmap
-
   get fa v = fa !! fromV v fa
 
   toV i fa = Vec (i `div` s) (i `mod` s) ()
