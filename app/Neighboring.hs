4b262effd42f212947a4198e2556a032840ebb84
wallace fares <wallacefares@gmail.com>
2022-11-28 16:59:43 -0300

    bruh

--- app/Neighboring.hs
+++ app/Neighboring.hs
@@ -16,13 +16,13 @@ data Neighboring = Neighboring
 neighboring :: Cell -> [Cell] -> Neighboring
 neighboring (Vec x y a) g = Neighboring t l r b all
   where
+    s = round $ size config
+    get x y = Grid.get g (Vec (x `mod` s) (y `mod` s) a)
     t = get x (y + 1)
     b = get x (y - 1)
     r = get (x + 1) y
     l = get (x - 1) y
     all = [t, l, r, b]
-    get x y = Grid.get g (Vec (x `mod` s) (y `mod` s) a)
-    s = round $ size config
 
 aliveNeighbors :: Cell -> [Cell] -> Int
 aliveNeighbors c = foldl (\b (Vec _ _ a) -> b + fromEnum a) 0 . all . neighboring c
--- lib/Grid.hs
+++ lib/Grid.hs
@@ -9,15 +9,15 @@ sqrtLength = round . sqrt . fromIntegral . length
 
 class (Foldable f, Functor g) => Grid f g where
   get :: Foldable f => f a -> g b -> a
-  fromV :: g a -> f b -> Int
+  toInt :: g a -> f b -> Int
   makeGrid :: Int -> f (g ())
 
 instance Functor Vec where
   fmap f (Vec x y a) = Vec x y (f a)
 
 instance Grid [] Vec where
-  get fa v = fa !! fromV v fa
+  get fa v = fa !! toInt v fa
 
-  fromV (Vec x y _) fa = sqrtLength fa * x + y
+  toInt (Vec x y _) fa = sqrtLength fa * x + y
 
   makeGrid n = [Vec x y () | x <- [1 .. n], y <- [1 .. n]]
